6c5a5d70c1825e3047ec3155eb4d4cef0295f804
Awakened-Redstone <awakenedredstone@gmail.com>
2022-10-18 21:42:21 -0300

    Code bump

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@
  */
 
 export interface Env {
+    TWITCH_AUTH: string;
+    TWITCH_CLIENT_ID: string;
     // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
     // MY_KV_NAMESPACE: KVNamespace;
     //
@@ -22,34 +24,53 @@ export interface Env {
 type Fetch = {
     pathReg: RegExp;
     methods: string[];
-    function: (request: Request) => Response;
+    handlers: ((request: RequestData) => Response | Promise<Response>)[];
+};
+
+type Obj = {
+    [propName: string]: string
 }
 
-const targets: Fetch[] = []
-const targetPaths: RegExp[] = []
+interface RequestData {
+    request: Request
+    env: Env
+    method: string
+    params?: Obj
+    query?: Obj
+    url: string
+}
+
+const targets: Fetch[] = [];
+const targetPaths: RegExp[] = [];
 
 export default {
     async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-        const path = new URL(request.url).pathname;
-        for (const target of targets) {
-            if ((target.methods.includes(request.method.toUpperCase()) || target.methods.includes('ALL')) && path.match(target.pathReg)) {
-                return target.function(request)
+        let response, match, url = new URL(request.url), requestData: RequestData = {request: request, env: env, method: request.method, url: request.url};
+        requestData.query = Object.fromEntries(url.searchParams)
+        for (const {pathReg, methods, handlers} of targets) {
+            if ((methods.includes(request.method.toUpperCase()) || methods.includes('ALL')) && (match = url.pathname.match(pathReg))) {
+                requestData.params = match.groups
+                for (const handler of handlers) {
+                    if ((response = await handler(requestData)) !== undefined) return response;
+                }
             }
         }
-        return new Response(`Invalid ${request.method.toUpperCase()} request!`);
+        return new Response(`Invalid ${request.method.toUpperCase()} request!`, {status: 404});
     }
 };
 
-function method(path: string, ...methods: string[]) {
+function CUSTOM(path: string, ...methods: string[]) {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        info(`Adding handler for path "${path.toString()}" for methods ${JSON.stringify(methods)}`)
+
         //Code from itty-router
         const regex = RegExp(`^${('' + path)
-            .replace(/(\/?)\*/g, '($1.*)?')                             // trailing wildcard
-            .replace(/(\/$)|((?<=\/)\/)/, '')                           // remove trailing slash or double slash from joins
-            .replace(/:(\w+)(\?)?(\.)?/g, '$2(?<$1>[^/]+)$2$3')         // named params
-            .replace(/\.(?=[\w(])/, '\\.')                              // dot in path
-            .replace(/\)\.\?\(([^\[]+)\[\^/g, '?)\\.?($1(?<=\\.)[^\\.') // optional image format
-        }/*$`)
+                .replace(/(\/?)\*/g, '($1.*)?')                             // trailing wildcard
+                .replace(/(\/$)|((?<=\/)\/)/, '')                           // remove trailing slash or double slash from joins
+                .replace(/:(\w+)(\?)?(\.)?/g, '$2(?<$1>[^/]+)$2$3')         // named params
+                .replace(/\.(?=[\w(])/, '\\.')                              // dot in path
+                .replace(/\)\.\?\(([^\[]+)\[\^/g, '?)\\.?($1(?<=\\.)[^\\.') // optional image format
+            }/*$`);
 
         if (targetPaths.includes(regex)) {
             const t = targets.find(value => value.pathReg === regex);
@@ -58,8 +79,8 @@ function method(path: string, ...methods: string[]) {
                 return;
             }
 
-            if (t.function !== descriptor.value) {
-                error("Illegal configuration! Multiple functions set to process the same path!")
+            if (!t.handlers.includes(descriptor.value)) {
+                t.handlers.push(descriptor.value);
             }
 
             for (const method of methods) {
@@ -72,74 +93,100 @@ function method(path: string, ...methods: string[]) {
             }
         }
 
-        targets.push({pathReg: regex, methods: methods, function: descriptor.value})
+        targetPaths.push(regex);
+        targets.push({pathReg: regex, methods: methods, handlers: [descriptor.value]})
     }
 }
 
 function GET(path: string) {
-    return method(path, "GET");
+    return CUSTOM(path, "GET");
 }
 
 function HEAD(path: string) {
-    return method(path, "HEAD");
+    return CUSTOM(path, "HEAD");
 }
 
 function POST(path: string) {
-    return method(path, "POST");
+    return CUSTOM(path, "POST");
 }
 
 function PUT(path: string) {
-    return method(path, "PUT");
+    return CUSTOM(path, "PUT");
 }
 
 function DELETE(path: string) {
-    return method(path, "DELETE");
+    return CUSTOM(path, "DELETE");
 }
 
 function CONNECT(path: string) {
-    return method(path, "CONNECT");
+    return CUSTOM(path, "CONNECT");
 }
 
 function OPTIONS(path: string) {
-    return method(path, "OPTIONS");
+    return CUSTOM(path, "OPTIONS");
 }
 
 function TRACE(path: string) {
-    return method(path, "TRACE");
+    return CUSTOM(path, "TRACE");
 }
 
 function PATCH(path: string) {
-    return method(path, "PATCH");
+    return CUSTOM(path, "PATCH");
 }
 
 function ALL(path: string) {
-    return method(path, "ALL");
+    return CUSTOM(path, "ALL");
 }
 
-function info(...data: any[]) {
-    const newData: any[] = ["FTM API [INFO]: "];
-    newData.push(data);
-    console.log(newData);
+function debug(data: any) {
+    console.info("FTM API [DEBUG]: ", data);
 }
 
-function warn(...data: any[]) {
-    const newData: any[] = ["FTM API [WARN]: "];
-    newData.push(data);
-    console.warn(newData);
+function info(data: any) {
+    console.info("FTM API [INFO]: ", data);
 }
 
-function error(...data: any[]) {
-    const newData: any[] = ["FTM API [ERROR]: "];
-    newData.push(data);
-    console.error(newData);
+function warn(data: any) {
+    console.warn("FTM API [WARN]: ", data);
 }
 
-class DummyClass {
-    @GET("/helloworld")
-    helloWorld(request: Request): Response {
+function error(data: any) {
+    console.info("FTM API [ERROR]: ", data);
+}
+
+class API {
+    @GET("/v1/*?")
+    removed(request: RequestData): Response {
+        return new Response("The API v1 has been deprecated and removed!")
+    }
+
+    @GET("/v2/helloworld")
+    helloWorld(request: RequestData): Response {
         return new Response("Hello world!");
     }
 
+    @GET("/v2/twitch/users/:user")
+    async users(request: RequestData): Promise<Response | undefined> {
+        const headers = {
+            headers: {
+                'Authorization': `Bearer ${request.env.TWITCH_AUTH}`,
+                'Client-Id': `${request.env.TWITCH_CLIENT_ID}`
+            }
+        }
+        const user = request.params.user;
+        const userValidate = RegExp("^[a-zA-Z\\d]\\w{0,24}$");
+        let search = "";
 
+        if (!request.query.type) {
+            if (parseInt(user)) search = `?id=${user}`;
+            else if (user.match(userValidate)) search = `?login=${user}`;
+            else return undefined; //TODO: change that
+        } else {
+            if (request.query.type === "login" || request.query.type === "user" && user.match(userValidate)) search = `?login=${user}`;
+            else if (request.query.type === "id") search = `?id=${user}`;
+            else return undefined; //TODO: change that
+        }
+        const url = `https://api.twitch.tv/helix/users${search}`
+        return fetch(url, headers);
+    }
 }
-
